571ccff36525e59fefcc5a00bef0e3b0a4ac0a61
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-02-17 14:57:30 +0300

    configured mongoDB and connected DB with route actions

--- app/api/route.js
+++ app/api/route.js
@@ -9,11 +9,29 @@ const loadDb = async () => {
 loadDb();
 
 export async function GET() {
-  return NextResponse.json({ message: "get method GEt" });
+  const projects = await ProjectModel.find({});
+
+  return NextResponse.json({ projects });
 }
 
 export async function POST(request) {
   const { title, description } = await request.json();
   await ProjectModel.create({ title, description });
   return NextResponse.json({ message: "Action created successfully" });
+}
+export async function DELETE(request) {
+  const url = new URL(request.url);
+  const mongoId = url.searchParams.get("mongoId");
+
+  await ProjectModel.findByIdAndDelete(mongoId);
+
+  return NextResponse.json({ message: "Action deleted successfully" });
+}
+export async function PUT(request) {
+  const url = new URL(request.url);
+  const mongoId = url.searchParams.get("mongoId");
+
+  await ProjectModel.findByIdAndUpdate(mongoId, { $set: { isCompleted: true } });
+
+  return NextResponse.json({ message: "Action completed successfully" });
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,33 @@
 "use client";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import { useSearchParams } from "next/navigation";
 export default function Home() {
+  const searchParams = useSearchParams();
+  const [projectData, setProjectData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({ title: "", description: "" });
 
   const onChangeHandler = (e) => {
     setFormData((formData) => ({ ...formData, [e.target.name]: e.target.value }));
   };
+
+  // fetching data from the server
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/api");
+      setProjectData(response.data.projects);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const hanldeSubmit = async (e) => {
     e.preventDefault();
@@ -15,18 +35,43 @@ export default function Home() {
       // api call
 
       const response = await axios.post("/api", formData);
+      setFormData({ title: "", description: "" });
 
       const notify = () => toast.success(response.data.message);
+      await fetchData();
       notify();
     } catch (error) {
       toast.error("Something went wrong");
     }
   };
+
+  // delete the action
+
+  const deleteAction = async (id) => {
+    const response = await axios.delete("/api", {
+      params: { mongoId: id },
+    });
+
+    toast.success(response.data.message);
+    fetchData();
+  };
+  const completeAction = async (id) => {
+    const response = await axios.put(
+      "/api",
+      {},
+      {
+        params: { mongoId: id },
+      }
+    );
+
+    toast.success(response.data.message);
+    fetchData();
+  };
   return (
     <>
       <ToastContainer
         position="top-right"
-        autoClose={5000}
+        autoClose={1000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick={false}
@@ -63,76 +108,84 @@ export default function Home() {
           </button>
         </div>
       </form>
-      <section className="w-[80%] max-w-[650px] mx-auto py-20">
-        <div role="status">
-          <svg
-            aria-hidden="true"
-            className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-orange-600"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span className="sr-only">Loading...</span>
-        </div>
-      </section>
-      <section className="w-[80%] max-w-[700px] mx-auto mt-12">
-        <div className="relative overflow-x-auto">
-          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="px-6 py-3">
-                  Id
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Title
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Description
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Status
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  1
-                </th>
-                <td className="px-6 py-4">Study</td>
-                <td className="px-6 py-4">Learn Next Js</td>
-                <td className="px-6 py-4">Pending</td>
-                <td className="px-6 py-4 flex items-center space-x-3">
-                  <button
-                    type="button"
-                    className="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-red-700 rounded-lg hover:bg-red-800    dark:bg-red-600 dark:hover:bg-red-700 "
-                  >
-                    Delete
-                  </button>
-                  <button
-                    type="button"
-                    className="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-green-700 rounded-lg hover:bg-green-800    dark:bg-green-600 dark:hover:bg-green-700 "
-                  >
-                    Done
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
+      {loading ? (
+        <section className="w-[80%] max-w-[650px] mx-auto py-20 flex items-center justify-center">
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-orange-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        </section>
+      ) : (
+        <section className="w-[80%] max-w-[1000px] mx-auto mt-12">
+          <div className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    Id
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Title
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Description
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Status
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {projectData.map((project) => (
+                  <tr key={project._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {project._id}
+                    </th>
+                    <td className={`px-6 py-4 ${project.isCompleted ? "line-through" : ""}`}>{project.title}</td>
+                    <td className={`px-6 py-4 ${project.isCompleted ? "line-through" : ""}`}>{project.description}</td>
+                    <td className="px-6 py-4">{project.isCompleted ? "Complete" : "Pending"}</td>
+                    <td className="px-6 py-4 flex items-center space-x-3">
+                      <button
+                        onClick={() => deleteAction(project._id)}
+                        type="button"
+                        className="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-red-700 rounded-lg hover:bg-red-800    dark:bg-red-600 dark:hover:bg-red-700 "
+                      >
+                        Delete
+                      </button>
+                      <button
+                        onClick={() => completeAction(project._id)}
+                        type="button"
+                        className="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-green-700 rounded-lg hover:bg-green-800    dark:bg-green-600 dark:hover:bg-green-700 "
+                        disabled={project.isCompleted && "disabled"}
+                      >
+                        Done
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      )}
     </>
   );
 }
